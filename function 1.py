88d82842cfdf9bc8c0c3ae97bfe9d78505e6bfb1
DineshV456 <dineshkumar.venkatesh@emergertech.com>
2025-09-30 12:17:52 +0530

    functions 1 file added

---
+++ function 1.py
@@ -0,0 +1,36 @@
+def add():
+    print("Addition")
+    a = int(input("Enter the number 1 :"))
+    b = int(input("Enter the number 2 :"))
+    c= a+b
+    print("The answer is, ", c)
+
+add()
+
+def subtract():
+    print("Subtraction")
+    a=int(input("Enter num 1 "))
+    b=int(input("Enter num 2 "))
+    c=a-b
+    print("The answer is ", c)
+
+subtract()
+
+def multiply():
+    print("Multiplication")
+    a=int(input("Enter num 1: "))
+    b=int(input("Enter num 2: "))
+    c=a*b
+    print("The answer is ", c)
+
+def divide():
+    print("Division")
+    a=int(input("Enter num 1 :"))
+    b=int(input("Enter num 2 :"))
+    c=a/b
+    print("The answer is ",c)
+
+
+multiply()
+divide()
+
