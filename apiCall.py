d24a69c34a9db0090be6ab7991f194b6e202537f
DineshV456 <dineshkumar.venkatesh@emergertech.com>
2025-10-23 15:56:18 +0530

    new changes

---
+++ apiCall.py
@@ -0,0 +1,13 @@
+import requests
+
+url = "https://api.restful-api.dev/objects"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+
+    print("API called successfully")
+    print(data)
+else:
+    print(f"Error {response.status_code}: {response.text}")    
