6290e474f6c1357129d6324cdd8e2a7869a8e4c4
DineshV456 <dineshkumar.venkatesh@emergertech.com>
2025-10-23 15:57:59 +0530

    edited the print

--- apiCall.py
+++ apiCall.py
@@ -7,7 +7,7 @@ response = requests.get(url)
 if response.status_code == 200:
     data = response.json()
 
-    print("API called successfully")
+    print("API called successfully...")
     print(data)
 else:
     print(f"Error {response.status_code}: {response.text}")    
